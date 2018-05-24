92aa2b322301e9b66c1f4a8c04f183257ce4c554
Dinesh <dineshd188@gmail.com>
2018-05-24 12:28:21 +0530

    Login page completed

--- app/src/main/java/com/example/welcome/hackathon/MainActivity.java
+++ app/src/main/java/com/example/welcome/hackathon/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.welcome.hackathon;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText email;
+    EditText pass;
+    Button but;
+    TextView signup;
+
+    FirebaseAuth auth;
+    ProgressDialog prg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try {
+            auth = FirebaseAuth.getInstance();
+            prg = new ProgressDialog(this);
+
+            if (auth.getCurrentUser() != null) {
+                finish();
+                startActivity(new Intent(this, Welcome.class));
+            }
+        }
+        catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        email = (EditText) findViewById(R.id.editTextEmail);
+        pass = (EditText) findViewById(R.id.editTextPassword);
+        but  = (Button) findViewById(R.id.buttonSignin);
+        signup = (TextView) findViewById(R.id.signup);
+
+        but.setOnClickListener(this);
+        signup.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==but){
+            startActivity(new Intent(this,Welcome.class));
+        }
     }
 }
